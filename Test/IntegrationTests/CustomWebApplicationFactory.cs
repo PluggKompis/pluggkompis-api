139ab2f221df57e5719c6a7dd6f8541c31bb532f
Mohanad <mohanad.aldaghestani@gmail.com>
2026-01-13 05:53:16 +0100

    Integration Test hotfix Added test jwt  settings

--- Test/IntegrationTests/CustomWebApplicationFactory.cs
+++ Test/IntegrationTests/CustomWebApplicationFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Test.IntegrationTests
@@ -14,23 +15,38 @@ namespace Test.IntegrationTests
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                // Provide required config for CI/testing (JWT is the common missing one)
+                var testConfig = new Dictionary<string, string?>
+                {
+                    ["Jwt:Issuer"] = "TestIssuer",
+                    ["Jwt:Audience"] = "TestAudience",
+                    // IMPORTANT: must be long enough for HMACSHA256
+                    ["Jwt:SecretKey"] = "THIS_IS_A_TEST_SECRET_KEY_THAT_IS_AT_LEAST_32_CHARS",
+                    ["Jwt:AccessTokenMinutes"] = "60"
+                };
+
+                config.AddInMemoryCollection(testConfig);
+            });
+
             builder.ConfigureServices(services =>
             {
-                // Remove existing DbContext
+                // Remove existing DbContext registration
                 var descriptor = services.SingleOrDefault(d =>
                     d.ServiceType == typeof(DbContextOptions<AppDbContext>));
 
                 if (descriptor != null)
                     services.Remove(descriptor);
 
-                // Create in-memory SQLite
+                // SQLite in-memory
                 _connection = new SqliteConnection("DataSource=:memory:");
                 _connection.Open();
 
                 services.AddDbContext<AppDbContext>(options =>
                     options.UseSqlite(_connection));
 
-                // Build provider and ensure DB created
+                // Ensure DB created
                 var sp = services.BuildServiceProvider();
                 using var scope = sp.CreateScope();
                 var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
